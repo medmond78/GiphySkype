fa4dbf397746f18f0bd11b7e343f763369c4cf9a
Matthew Edmond <medmond95@gmail.com>
2016-04-12 16:32:36 -0400

    reverted to api call

--- echo.js
+++ echo.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 const restify = require('restify');
 const skype = require('skype-sdk');
-const giphy = require('giphy-api')();
+//const giphy = require('giphy-api')();
 
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const synchronousRequest = new XMLHttpRequest();
 //synchronousRequest.open('GET', 'https://randomapi.com/api/?key=XY96-764H-K6J1-UBDR&id=k1nbk4p&noinfo', false); // false means synchronous.
 //synchronousRequest.send();
 
-const baseURL= 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=';
 
 //var obj = JSON.parse(synchronousRequest.responseText);
 //console.log("First Name:", obj['results']['0']['object']['first_name']);
@@ -30,19 +29,15 @@ botService.on('contactAdded', (bot, data) => {
 
 botService.on('personalMessage', (bot, data) => {
 
-  giphy.random({
-    tag: 'magic',
-    fmt: 'json'
-    }, function(err, res) {
-  });
-
-  var obj = JSON.parse(res);
+  synchronousRequest.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=magic', false); // false means synchronous.
+  synchronousRequest.send();
+  var obj = JSON.parse(synchronousRequest.responseText);
 
   //Reply
   //bot.reply(`Hey ${obj['data']['url']}. Thank you for your message: "${data.content}".`, true);
 
-  //bot.reply(`${obj['data']['image_original_url']}`, true);
-  bot.reply(`${res}`, true);
+  bot.reply(`${obj['data']['image_original_url']}`, true);
+  //bot.reply(`${res}`, true);
 
 });
 
