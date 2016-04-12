26d27ee7fb549abf042f21e71235fa7bc99ac7f4
Matthew Edmond <medmond95@gmail.com>
2016-04-12 14:22:52 -0400

    d

--- echo.js
+++ echo.js
@@ -7,7 +7,8 @@ const synchronousRequest = new XMLHttpRequest();
 //synchronousRequest.open('GET', 'https://randomapi.com/api/?key=XY96-764H-K6J1-UBDR&id=k1nbk4p&noinfo', false); // false means synchronous.
 //synchronousRequest.send();
 
-const mainURL = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=magic';
+const mainURL = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=';
+const tag = 'magic';
 
 //var obj = JSON.parse(synchronousRequest.responseText);
 //console.log("First Name:", obj['results']['0']['object']['first_name']);
@@ -31,7 +32,8 @@ botService.on('personalMessage', (bot, data) => {
 
   //call name API
   //synchronousRequest.open('GET', 'https://randomapi.com/api/?key=XY96-764H-K6J1-UBDR&id=k1nbk4p&noinfo', false); // false means synchronous.
-  synchronousRequest.open('GET', mainURL, false); // false means synchronous.
+  var submitURL = url.resolve(mainURL, tag);
+  synchronousRequest.open('GET', submitURL, false); // false means synchronous.
   synchronousRequest.send();
   var obj = JSON.parse(synchronousRequest.responseText);
 
